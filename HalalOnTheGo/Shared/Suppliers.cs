39742aff697e81ca5bec366c898892ece1ee7e49
Amaan-Nazir <78367439+Amaan-Nazir@users.noreply.github.com>
2024-02-13 13:06:07 +0000

    PROGRESS MADE - adding images, servicesm pages, shared folder etc.

---
+++ HalalOnTheGo/Shared/Suppliers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalalOnTheGo.Shared
+{
+    public class Suppliers
+    {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string Location { get; set; }
+
+    }
+}
