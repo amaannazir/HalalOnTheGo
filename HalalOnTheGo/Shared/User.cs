cbbabdcb3d9e48df19802bab969c664eb451076b
Amaan-Nazir <78367439+Amaan-Nazir@users.noreply.github.com>
2024-03-10 18:25:36 +0000

    Adding login page, and starting user authentication. Adding new package for authentication. Login form created with email and password.

---
+++ HalalOnTheGo/Shared/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalalOnTheGo.Shared
+{
+    public class User //User model to be used for login authentication.
+    {
+        public int Id { get; set; }
+        public string Email{ get; set; } //User UNIVERSITY EMAIL for authentication - in form
+        public string Password{ get; set; }//To be used in form 
+    }
+}
