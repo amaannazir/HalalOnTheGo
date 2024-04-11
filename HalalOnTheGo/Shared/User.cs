10d502c6a20e827041e561ed5f3ae6d4bf008661
Amaan-Nazir <78367439+Amaan-Nazir@users.noreply.github.com>
2024-04-11 17:42:49 +0100

    sign up page and all validation

--- HalalOnTheGo/Shared/User.cs
+++ HalalOnTheGo/Shared/User.cs
@@ -8,8 +8,13 @@ namespace HalalOnTheGo.Shared
 {
     public class User //User model to be used for login authentication.
     {
-        public int Id { get; set; }
-        public string Email{ get; set; } //User UNIVERSITY EMAIL for authentication - in form
+        
+        public string StudentEmail{ get; set; } //User UNIVERSITY EMAIL for authentication - in form
         public string Password{ get; set; }//To be used in form 
+        public string FirstName { get; set; }//To be used in SIGN UP form 
+        public string LastName { get; set; } //To be used in SIGN UP form 
+        public string University { get; set; } //To be used in SIGN UP form 
+        public string ConfirmPassword { get; set; } //Validating password
+
     }
 }
